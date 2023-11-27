8ace14dbdd430d15879453d6dc6425ecfc1ae044
Salvatore <mammoliti.sam@gmail.com>
2023-11-27 14:35:33 -0500

    commented style sheet, index and script files

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+// Event listner set up
 document.addEventListener("DOMContentLoaded", function () {
   var generateBtn = document.querySelector("#generate");
 
+//   Password generator function
   function writePassword() {
       var password = generatePassword();
       var passwordText = document.querySelector("#password");
@@ -8,6 +10,7 @@ document.addEventListener("DOMContentLoaded", function () {
       passwordText.value = password;
   }
 
+//   Password generator function to get length and special characters of password
   function generatePassword() {
       var length = document.getElementById("length").value;
       var includeLowercase = document.getElementById("lowercase").checked;
@@ -15,11 +18,13 @@ document.addEventListener("DOMContentLoaded", function () {
       var includeNumbers = document.getElementById("numbers").checked;
       var includeSpecial = document.getElementById("special").checked;
 
+    //   Validate password length 8-128 characters
       if (length < 8 || length > 128) {
           alert("Invalid password length. Please enter a length between 8 and 128 characters.");
           return "";
       }
 
+    //   Character definitions
       const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
       const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
       const numberChars = "0123456789";
@@ -39,21 +44,22 @@ document.addEventListener("DOMContentLoaded", function () {
           allChars += specialChars;
       }
 
+    //   To check that at least one character type has been selected
       if (allChars === "") {
           alert("Please select at least one character type.");
           return "";
       }
 
+    //   Generate password
       let password = "";
       for (let i = 0; i < length; i++) {
           const randomIndex = Math.floor(Math.random() * allChars.length);
           password += allChars.charAt(randomIndex);
       }
 
+    //   Return generated password
       return "Generated Password: " + password;
   }
 
   generateBtn.addEventListener("click", writePassword);
 });
-
-generateBtn.addEventListener("click"), (writePassword);
